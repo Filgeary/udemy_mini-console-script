901eb3797833e4b55618ce806cf139df32a5148f
Georgiy Filippov <g.filippov777@gmail.com>
2020-07-26 22:00:10 +0500

    adds questions about last films
    adds also film's rates

--- src/app.js
+++ src/app.js
@@ -34,3 +34,14 @@ const personalMovieDB = {
 };
 
 personalMovieDB.count = numberOfFilms;
+
+const firstQuestionLastFilm = prompt('What the last movie have you watched?');
+const firstQuestionRateFilm = prompt('How do you rate this film?');
+
+const secondQuestionLastFilm = prompt('What the last movie have you watched?');
+const secondQuestionRateFilm = prompt('How do you rate this film?');
+
+personalMovieDB.movies[firstQuestionLastFilm] = firstQuestionRateFilm;
+personalMovieDB.movies[secondQuestionLastFilm] = secondQuestionRateFilm;
+
+console.log(personalMovieDB);
