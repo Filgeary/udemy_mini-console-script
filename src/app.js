2db2a443d72986b43769c1a0fdd5661189825260
Georgiy Filippov <g.filippov777@gmail.com>
2020-07-28 18:39:13 +0500

    add check for user's answer about LastFilm

--- src/app.js
+++ src/app.js
@@ -24,8 +24,20 @@ const personalMovieDB = {
   private: false,
 };
 
-for (let i = 0; i < 3; i++) {
+start: for (let i = 0; i < 3; i++) {
   let answerLastFilm = prompt('What the last movie have you watched?');
+  let count = 0;
+
+  while (!answerLastFilm || answerLastFilm.length > 50) {
+    answerLastFilm = prompt('Repeat: What the last movie have you watched?');
+    count++;
+
+    if (count >= 3) {
+      alert(`Begin from Start!`);
+      break start;
+    }
+  }
+
   let answerRateFilm = prompt('How do you rate this film?');
 
   personalMovieDB.movies[answerLastFilm] = answerRateFilm;
