948b07acc55676d7d2a9fb6e35beccc6c4ebc292
Georgiy Filippov <g.filippov777@gmail.com>
2021-03-04 02:26:16 +0500

    delete comments

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
-/* Задание на урок:
-
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство private. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
-
 'use strict';
 
 const personalMovieDB = {
@@ -50,7 +36,7 @@ const personalMovieDB = {
       const favoriteUserGenre = prompt(
         `Your favorite genre by number ${i + 1}`
       );
-      if (favoriteUserGenre) {
+      if (favoriteUserGenre && !isFinite(favoriteUserGenre)) {
         personalMovieDB.genres[i] = favoriteUserGenre;
         console.log(`done`);
       } else {
