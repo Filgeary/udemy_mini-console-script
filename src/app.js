2a6449a10d64c7338eab25e0c94d1870e46bc2ad
Georgiy Filippov <g.filippov777@gmail.com>
2021-03-04 00:27:51 +0500

    refactoring - add methods to 'personalMovieDB'

--- src/app.js
+++ src/app.js
@@ -20,58 +20,59 @@ const personalMovieDB = {
   actors: {},
   genres: [],
   private: false,
+  getFilmsAmount() {
+    let numberOfFilms = prompt(`How many movies have you watched?`);
+
+    while (!numberOfFilms || !isFinite(numberOfFilms)) {
+      numberOfFilms = +prompt(`How many movies have you watched?`);
+    }
+    personalMovieDB.count = numberOfFilms;
+  },
+  getFilmsRatingList() {
+    for (let i = 0; i < 2; i++) {
+      const answerLastFilm = prompt(`What the last movie have you watched?`);
+      const answerRateFilm = prompt(
+        `How do you rate this film?`,
+        `from 1 to 10`
+      );
+
+      if (answerLastFilm && answerLastFilm.length < 50 && answerRateFilm) {
+        personalMovieDB.movies[answerLastFilm] = answerRateFilm;
+        console.log(`done`);
+      } else {
+        console.log(`error`);
+        i--;
+      }
+    }
+  },
+  getUserGenres() {
+    for (let i = 0; i < 3; i++) {
+      const favoriteUserGenre = prompt(
+        `Your favorite genre by number ${i + 1}`
+      );
+      personalMovieDB.genres[i] = favoriteUserGenre;
+    }
+  },
+  showUserRating() {
+    if (personalMovieDB.count < 10) {
+      alert(`You watched so few movies`);
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      alert(`You are ordinary person`);
+    } else if (personalMovieDB.count > 30) {
+      alert(`You are a movie Fan!`);
+    } else {
+      alert(`ERROR`);
+    }
+  },
+  showMyDB() {
+    if (!personalMovieDB.private) {
+      console.log(personalMovieDB);
+    }
+  },
 };
 
-function getFilmsAmount() {
-  let numberOfFilms = prompt(`How many movies have you watched?`);
-
-  while (!numberOfFilms || !isFinite(numberOfFilms)) {
-    numberOfFilms = +prompt(`How many movies have you watched?`);
-  }
-  personalMovieDB.count = numberOfFilms;
-}
-getFilmsAmount();
-
-function getFilmsRatingList() {
-  for (let i = 0; i < 2; i++) {
-    const answerLastFilm = prompt(`What the last movie have you watched?`);
-    const answerRateFilm = prompt(`How do you rate this film?`, `from 1 to 10`);
-
-    if (answerLastFilm && answerLastFilm.length < 50 && answerRateFilm) {
-      personalMovieDB.movies[answerLastFilm] = answerRateFilm;
-      console.log(`done`);
-    } else {
-      console.log(`error`);
-      i--;
-    }
-  }
-}
-getFilmsRatingList();
-
-function getUserGenres() {
-  for (let i = 0; i < 3; i++) {
-    const favoriteUserGenre = prompt(`Your favorite genre by number ${i + 1}`);
-    personalMovieDB.genres[i] = favoriteUserGenre;
-  }
-}
-getUserGenres();
-
-function showUserRating() {
-  if (personalMovieDB.count < 10) {
-    alert(`You watched so few movies`);
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert(`You are ordinary person`);
-  } else if (personalMovieDB.count > 30) {
-    alert(`You are a movie Fan!`);
-  } else {
-    alert(`ERROR`);
-  }
-}
-showUserRating();
-
-function showMyDB() {
-  if (!personalMovieDB.private) {
-    console.log(personalMovieDB);
-  }
-}
-showMyDB();
+personalMovieDB.getFilmsAmount();
+personalMovieDB.getFilmsRatingList();
+personalMovieDB.getUserGenres();
+personalMovieDB.showUserRating();
+personalMovieDB.showMyDB();
